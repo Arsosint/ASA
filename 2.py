ff8d8c0a423905f343cf9ec13dc151bff4d35543
Arsosint <arsennnnn100513@gmail.com>
2024-11-28 14:35:12 +0500

    Update 2.py

--- 2.py
+++ 2.py
@@ -7,7 +7,7 @@ def random_name(length=10):
     letters = string.ascii_lowercase  # Используем только маленькие буквы
     return ''.join(random.choice(letters) for _ in range(length))
 
-def create_bot(server_address, version):
+def create_bot(server_address, port, version):
     bot = Bot(random_name(), version=version)
     
     @bot.event
@@ -15,17 +15,18 @@ def create_bot(server_address, version):
         while True:
             bot.chat("привет")
             bot.jump()
-            time.sleep(0.1) 
+            time.sleep(0.5)  # Задержка, чтобы избежать спама
 
-    bot.connect(server_address)
+    bot.connect(server_address, port)
 
 def main():
     server_address = input("Выберите адрес сервера: ")
+    port = int(input("Введите порт сервера: "))
     version = input("Выберите версию Minecraft (например, '1.16.4'): ")
     num_bots = int(input("Выберите количество ботов: "))
 
     for _ in range(num_bots):
-        create_bot(server_address, version)
+        create_bot(server_address, port, version)
 
 if __name__ == "__main__":
     main()
